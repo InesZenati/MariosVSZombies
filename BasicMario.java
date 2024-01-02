c9c4de1995f8c3e9e98f71fa827e79a13f1d4e96
InesZenati <ineshanna31@gmail.com>
2024-01-02 16:25:18 +0100

    nv BasicMario

--- BasicMario.java
+++ BasicMario.java
@@ -1,14 +1,8 @@
 public class BasicMario extends Mario{
-
-    BasicMario(int x, int y){
-        super("BasicMario",10,new Information(2,0,x,y,10));
+    BasicMario(){
+        super("BasicMario",10,new Information(2,0,-1,-1,10));
     }
     public void attaque(Zombie z){
        
     }
-
-    public Information getInfo(){
-        return super.getInfoInitale();
-    }
-    
 }
