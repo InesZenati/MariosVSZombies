f350875282ac533d00b87e2ba9679a77a6c305da
InesZenati <ineshanna31@gmail.com>
2024-01-02 16:26:03 +0100

    nv Mario

--- Mario.java
+++ Mario.java
@@ -20,4 +20,16 @@ public abstract class Mario extends Personnage {
         return this.getName() + " \n Point de vie :" + this.getInfoActuelle().getvie() + " \n Attaque :" + this.getInfoActuelle().getAttaque() + " \n Defense :" + this.getInfoActuelle().getDefense()+ " \n Prix :" + this.prix;
     }
 
+    public boolean peutAttaquer(Plateau p){
+        int [] pos = this.getPosition();
+        if(pos[0]!=0){
+            for(int i=pos[1]; i<p.getNumCols()-1;i++){
+                if(p.getCase(pos[0], i).contientZombie()){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
 }
