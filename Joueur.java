22c1a9ff555e17c0f04de21ebb5936ced87a45e0
InesZenati <ineshanna31@gmail.com>
2024-01-02 14:19:38 +0100

    nv joueur

--- Joueur.java
+++ Joueur.java
@@ -1,6 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Joueur {
     private final String name;
-    private int argent =0;
+    private int argent =30;
     private int score;
 
     public Joueur(String name, int argent, int score) {
@@ -37,4 +41,36 @@ public class Joueur {
         this.score += montant;
     }
 
+    public void afficherMessage(String message) {
+        System.out.println(message);
+    }
+
+        public void afficherMArioDisponibles(List<Mario> MArioDisponibles) {
+        afficherMessage("Mario disponibles :");
+        for (int i = 0; i < MArioDisponibles.size(); i++) {
+            Mario mario = MArioDisponibles.get(i);
+            if(mario.getPrix()<=this.argent){
+                afficherMessage((i + 1) + ". " + mario.toString());
+            }
+        }
+    }
+    public String demanderPersoPosition(){
+        Scanner scanner = new Scanner(System.in);
+        afficherMessage("Tour: ");
+        String position = scanner.nextLine();
+        return position;
+    }
+
+    public static void main(String[] args) {
+         List<Mario> MArioDisponibles =new ArrayList<>();
+        MArioDisponibles.add(new BasicMario( 0, 0));
+        MArioDisponibles.add(new WallBrick( 0, 0));
+        MArioDisponibles.add(new FireMario(0, 0));
+        MArioDisponibles.add(new BigMario(0, 0));
+        MArioDisponibles.add(new StarMario(0, 0));
+        Joueur joueur = new Joueur("joueur", 30, 0);
+        joueur.afficherMArioDisponibles(MArioDisponibles);
+        
+
+    }
 }
