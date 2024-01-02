680e45bc815432ae0f7e47b40c803ee7133c990c
InesZenati <ineshanna31@gmail.com>
2024-01-02 15:01:44 +0100

    nv zombie

--- Zombie.java
+++ Zombie.java
@@ -1,9 +1,7 @@
 public class Zombie extends Personnage {
     private final int gain;
     private int vitesse;
-   // private final Information InfoInitiale;
     private final Information InfoInitiale;
-
 
     Zombie(String name,int g, int v, Information i){
         super(false,name);
@@ -41,22 +39,11 @@ public class Zombie extends Personnage {
          this.getInfoActuelle().getDefense()+" \n Vitesse :"+v+" \n Gain :"+this.gain;
     }
 
-    public void spawn(Plateau p){
-         int i = p.getNumCols()-1;
-        int j = 1+(int)(Math.random() * ((p.getNumLi()-1) ));
-        while (p.getCase(i,j).getPerso() != null){
-         i = p.getNumCols()-1;
-         j = 1+(int)(Math.random() * ((p.getNumLi()-1) ));
-        // p.getNumCols()-1;
-        }
-        this.InfoInitiale.setPosX(i);
-        this.InfoInitiale.setPosY(j);
-        p.getCase(i,j).setZombie(this);
-        
-    }
+
     public void takeDamage(int degat){
         this.getInfoActuelle().setvie(this.getInfoActuelle().getvie() - degat);
     }
     
 } 
     
+
