252057877eabb45a59bec591354305d57b3a3e0a
InesZenati <ineshanna31@gmail.com>
2024-01-02 16:19:51 +0100

    nv zombie2

--- Zombie2.java
+++ Zombie2.java
@@ -1,5 +1,5 @@
 public class Zombie2 extends Zombie {
-    public Zombie2(int x,int y,int v){
-        super("ZombieAmélioré",30,v,new Information(2,4,x,y,10));
+    public Zombie2(int v){
+        super("ZombieAmélioré",30,v,new Information(2,4,-1,-1,10));
     }
 }
