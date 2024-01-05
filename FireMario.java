922dd887581109b9f28d20eab7de77065e3f5e6d
Mayu <129101290+111ayu@users.noreply.github.com>
2024-01-05 09:33:54 +0000

    attaqueMario

--- FireMario.java
+++ FireMario.java
@@ -1,129 +1,34 @@
 public class FireMario  extends Mario{
 
     public FireMario(){
-        super("FireMArio",200,new Information(10,0,-1,-1,100));
-    }
-    public void takeDamage(int degat){
-        this.getInfoActuelle().setvie(this.getInfoActuelle().getvie() - degat);
+        super("FireMario",200,new Information(10,0,-1,-1,100));
     }
 
     public void attaque(Plateau p){
         int [] pos = this.getPosition();
         if(p.getCase(pos[0],pos[1]+1).contientZombie()){
-            if(p.getCase(pos[0],pos[1]+1).getPersonnage().getName()=="ZombieSimple"){
-                Zombie1 m = (Zombie1) p.getCase(pos[0],pos[1]+1).getPersonnage();
+                Personnage m = p.getCase(pos[0],pos[1]+1).getPersonnage();
                 if(m.getInfoActuelle().getDefense()>this.getInfoActuelle().getAttaque()){
                     m.takeDamage(this.getInfoActuelle().getAttaque()/2);
                 }
                 else{
                     m.takeDamage(this.getInfoActuelle().getAttaque());
                 }
+        }else if(p.getCase(pos[0],pos[1]+2).contientZombie()){
+            Personnage m1 = p.getCase(pos[0],pos[1]+2).getPersonnage();
+            if(m1.getInfoActuelle().getDefense()>this.getInfoActuelle().getAttaque()){
+                m1.takeDamage(this.getInfoActuelle().getAttaque()/2);
+            }else{
+                m1.takeDamage(this.getInfoActuelle().getAttaque());
+            }        
+        }else if(p.getCase(pos[0],pos[1]+3).contientZombie()){
+            Personnage p3 =p.getCase(pos[0],pos[1]+3).getPersonnage();
+            if(p3.getInfoActuelle().getDefense()>this.getInfoActuelle().getAttaque()){
+                p3.takeDamage(this.getInfoActuelle().getAttaque()/2);
+            }else{
+                p3.takeDamage(this.getInfoActuelle().getAttaque());
             }
-            else if(p.getCase(pos[0],pos[1]+1).getPersonnage().getName()=="ZombieAmélioré"){
-                Zombie2 m = (Zombie2) p.getCase(pos[0],pos[1]+1).getPersonnage();
-                if(m.getInfoActuelle().getDefense()>this.getInfoActuelle().getAttaque()){
-                    m.takeDamage(this.getInfoActuelle().getAttaque()/2);
-                }
-                else{
-                    m.takeDamage(this.getInfoActuelle().getAttaque());
-                }
-            }
-            else if(p.getCase(pos[0],pos[1]+1).getPersonnage().getName()=="SuperZombie"){
-                Zombie3 m = (Zombie3) p.getCase(pos[0],pos[1]+1).getPersonnage();
-                if(m.getInfoActuelle().getDefense()>this.getInfoActuelle().getAttaque()){
-                    m.takeDamage(this.getInfoActuelle().getAttaque()/2);
-                }
-                else{
-                    m.takeDamage(this.getInfoActuelle().getAttaque());
-                }
-            }
-            else if(p.getCase(pos[0],pos[1]+1).getPersonnage().getName()=="SuperMegaUltraZombie"){
-                Zombie4 m = (Zombie4) p.getCase(pos[0],pos[1]+1).getPersonnage();
-                if(m.getInfoActuelle().getDefense()>this.getInfoActuelle().getAttaque()){
-                    m.takeDamage(this.getInfoActuelle().getAttaque()/2);
-                }
-                else{
-                    m.takeDamage(this.getInfoActuelle().getAttaque());
-                }
-            }
-        }
-        if(p.getCase(pos[0],pos[1]+2).contientZombie()){
-
-            if(p.getCase(pos[0],pos[1]+2).getPersonnage().getName()=="ZombieSimple"){
-                Zombie1 m = (Zombie1) p.getCase(pos[0],pos[1]+2).getPersonnage();
-                if(m.getInfoActuelle().getDefense()>this.getInfoActuelle().getAttaque()){
-                    m.takeDamage(this.getInfoActuelle().getAttaque()/2);
-                }
-                else{
-                    m.takeDamage(this.getInfoActuelle().getAttaque());
-                }
-            }
-            else if(p.getCase(pos[0],pos[1]+2).getPersonnage().getName()=="ZombieAmélioré"){
-                Zombie2 m = (Zombie2) p.getCase(pos[0],pos[1]+2).getPersonnage();
-                if(m.getInfoActuelle().getDefense()>this.getInfoActuelle().getAttaque()){
-                    m.takeDamage(this.getInfoActuelle().getAttaque()/2);
-                }
-                else{
-                    m.takeDamage(this.getInfoActuelle().getAttaque());
-                }
-            }
-            else if(p.getCase(pos[0],pos[1]+2).getPersonnage().getName()=="SuperZombie"){
-                Zombie3 m = (Zombie3) p.getCase(pos[0],pos[1]+2).getPersonnage();
-                if(m.getInfoActuelle().getDefense()>this.getInfoActuelle().getAttaque()){
-                    m.takeDamage(this.getInfoActuelle().getAttaque()/2);
-                }
-                else{
-                    m.takeDamage(this.getInfoActuelle().getAttaque());
-                }
-            }
-            else if(p.getCase(pos[0],pos[1]+2).getPersonnage().getName()=="SuperMegaUltraZombie"){
-                Zombie4 m = (Zombie4) p.getCase(pos[0],pos[1]+2).getPersonnage();
-                if(m.getInfoActuelle().getDefense()>this.getInfoActuelle().getAttaque()){
-                    m.takeDamage(this.getInfoActuelle().getAttaque()/2);
-                }
-                else{
-                    m.takeDamage(this.getInfoActuelle().getAttaque());
-                }
-            }
-        }
-        if(p.getCase(pos[0],pos[1]+3).contientZombie()){
-            if(p.getCase(pos[0],pos[1]+3).getPersonnage().getName()=="ZombieSimple"){
-                Zombie1 m = (Zombie1) p.getCase(pos[0],pos[1]+3).getPersonnage();
-                if(m.getInfoActuelle().getDefense()>this.getInfoActuelle().getAttaque()){
-                    m.takeDamage(this.getInfoActuelle().getAttaque()/2);
-                }
-                else{
-                    m.takeDamage(this.getInfoActuelle().getAttaque());
-                }
-            }
-            else if(p.getCase(pos[0],pos[1]+3).getPersonnage().getName()=="ZombieAmélioré"){
-                Zombie2 m = (Zombie2) p.getCase(pos[0],pos[1]+3).getPersonnage();
-                if(m.getInfoActuelle().getDefense()>this.getInfoActuelle().getAttaque()){
-                    m.takeDamage(this.getInfoActuelle().getAttaque()/2);
-                }
-                else{
-                    m.takeDamage(this.getInfoActuelle().getAttaque());
-                }
-            }
-            else if(p.getCase(pos[0],pos[1]+3).getPersonnage().getName()=="SuperZombie"){
-                Zombie3 m = (Zombie3) p.getCase(pos[0],pos[1]+3).getPersonnage();
-                if(m.getInfoActuelle().getDefense()>this.getInfoActuelle().getAttaque()){
-                    m.takeDamage(this.getInfoActuelle().getAttaque()/2);
-                }
-                else{
-                    m.takeDamage(this.getInfoActuelle().getAttaque());
-                }
-            }
-            else if(p.getCase(pos[0],pos[1]+3).getPersonnage().getName()=="SuperMegaUltraZombie"){
-                Zombie4 m = (Zombie4) p.getCase(pos[0],pos[1]+3).getPersonnage();
-                if(m.getInfoActuelle().getDefense()>this.getInfoActuelle().getAttaque()){
-                    m.takeDamage(this.getInfoActuelle().getAttaque()/2);
-                }
-                else{
-                    m.takeDamage(this.getInfoActuelle().getAttaque());
-                }
-            }
-        }
+        }       
     }
    
 }
--- WallBrick.java
+++ WallBrick.java
@@ -2,7 +2,6 @@ public class WallBrick extends Mario{
     WallBrick(){
         super("WallBrick",0,new Information(0,0,-1,-1,500));
     }
-    public void takeDamage(int degat){
-        this.getInfoActuelle().setvie(this.getInfoActuelle().getvie() - degat);
+    public void attaque(Plateau p){
     }
 }
